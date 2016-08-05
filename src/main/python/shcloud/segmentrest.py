0c71b4ed3e85b894861e561928de3b420a7be21f
zhuanghua <30758919@qq.com>
2016-08-05 14:57:23 +0800

    first commit

---
+++ src/main/python/shcloud/segmentrest.py
@@ -0,0 +1,24 @@
+'''
+Created on Jul 10, 2016
+
+@author: aadebuger
+'''
+
+from flask import Flask
+from flask import request
+app = Flask(__name__)
+
+def logBatchitem(item):
+	print(item)
+def logBatch( batchv):
+	map(lambda item:logBatchitem(item),batchv)
+@app.route("/v1/batch",methods=['GET', 'POST'])
+def hello():
+    print("data=",request.json)
+
+    batch = request.json['batch']
+    logBatch(batch)
+    return """{"status":"Hello World!"}"""
+
+if __name__ == "__main__":
+    app.run()
